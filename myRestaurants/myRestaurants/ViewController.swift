cb08746cc5674a9623b69728710e3e35bef6ea60
Araceli Teixeira <aracelinteixeira@gmail.com>
2017-10-18 05:30:13 -0400

    Creating project

---
+++ myRestaurants/myRestaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myRestaurants
+//
+//  Created by MacStudent on 2017-10-18.
+//  Copyright © 2017 Araceli Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
